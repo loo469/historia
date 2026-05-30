f64c1a187a62821946a9d15c9a6d2a6a719e1748
loo469 <loo469@users.noreply.github.com>
2026-05-30 10:51:20 +0200

    Delta: show earliest residual clear action

--- test/ui/intrigue/webIntrigueTurnReportDeltas.test.js
+++ test/ui/intrigue/webIntrigueTurnReportDeltas.test.js
@@ -70,6 +70,8 @@ test('playable map wires intrigue aftermath deltas into selected province turn r
   assert.match(webAppSource, /Nettoyage trace:/);
   assert.match(webAppSource, /Prochain check:/);
   assert.match(webAppSource, /nextCheck/);
+  assert.match(webAppSource, /Premier clear:/);
+  assert.match(webAppSource, /earliestClear/);
   assert.match(webAppSource, /province-intrigue-turn-report__residual-clear-action--/);
   assert.match(webAppSource, /province-intrigue-turn-report__return-trigger--/);
   assert.match(webAppSource, /province-intrigue-turn-report__skip-stabilization-risk--/);
